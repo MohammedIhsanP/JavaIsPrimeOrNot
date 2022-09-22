726bb409f46ac4ed10980969bc474d2398abd008
MohammedIhsanP <ihsudarulaman@gmail.com>
2022-09-22 20:37:24 +0530

    isPrimeOrNot added

---
+++ src/isPrimeOrNot.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class isPrimeOrNot {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number:");
+        int num= sc.nextInt();
+
+        boolean flag = true;
+
+        for (int i=2; i<=num/2 ; i++){
+            if (num%i==0){
+                flag = false;
+                break;
+            }
+        }
+        if (flag){
+            System.out.println("The number is Prime.");
+        }
+        else {
+            System.out.println("The number is not Prime.");
+        }
+    }
+}
